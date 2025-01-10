58e31dcbf92300723b96dc64f7fcdf5559f7757c
Said00510 <dylanrivas200206@gmail.com>
2025-01-10 18:08:33 -0500

    Ocultar de momento la fecha en la que se creara la tarea

--- src/components/DrawTask/DrawTask.jsx
+++ src/components/DrawTask/DrawTask.jsx
@@ -2,18 +2,19 @@ import { CheckBox } from "./DrawCheckBox";
 import Trash from "../../assets/svg/trash.svg";
 import PropTypes from "prop-types";
 
-export function DrawTask({ value, deleteTask, handleCompleted, completedTasks, taskCompleted }) {
+export function DrawTask({ value, taskCompleted, deleteTask, handleCompleted, completedTasks, handleDeleteCompletedTask }) {
 
-  const isCompleted = completedTasks.includes(value) ? "line-through" : "";
-  const isCompletedButton = completedTasks.includes(value) ? "opacity-0" : "";
+  // const date = new Date();
+  const isCompleted = completedTasks?.includes(value)? "line-through" : "";
+  const isCompletedButton = completedTasks?.includes(value) ? "opacity-0" : "";
 
   return (
     <>
-      <div className="bg-[#393B3B] p-3 flex gap-8 my-4 rounded-lg text-white">
+      <div className="bg-[#393B3B] p-3 flex gap-x-3 my-4 rounded-lg text-white max-w-full items-center">
         <div className="flex flex-col gap-8">
           <button
             onClick={handleCompleted}   
-            className={`flex justify-center items-center`}
+            className={`flex justify-center items-center w-[20px]`}
           >
             <CheckBox  />
           </button>
@@ -28,9 +29,9 @@ export function DrawTask({ value, deleteTask, handleCompleted, completedTasks, t
             </button>
 
         </div>
-        <div>
-          <p className={`${taskCompleted ? " " : isCompleted }`}>{value}</p>
-          <small>Tareas</small>
+        <div className="max-w-full">
+          <p className={`break-words whitespace-normal pr-4 ${taskCompleted ? "" : isCompleted }`}>{value}</p>
+          <small>Tareas </small>
         </div>
       </div>
     </>
@@ -38,11 +39,11 @@ export function DrawTask({ value, deleteTask, handleCompleted, completedTasks, t
 }
 
 
-DrawTask.propTypes = {
-  value: PropTypes.string.isRequired,
-  deleteTask: PropTypes.func.isRequired,
-  handleCompleted: PropTypes.func.isRequired,
-  completedTasks: PropTypes.array.isRequired,
-  handleDeleteCompletedTask: PropTypes.func.isRequired,
-  taskCompleted: PropTypes.string.isRequired,
-};
+// DrawTask.propTypes = {
+//   value: PropTypes.string.isRequired,
+//   deleteTask: PropTypes.func.isRequired,
+//   handleCompleted: PropTypes.func.isRequired,
+//   completedTasks: PropTypes.array.isRequired,
+//   handleDeleteCompletedTask: PropTypes.func.isRequired,
+//   taskCompleted: PropTypes.string.isRequired,
+// };
