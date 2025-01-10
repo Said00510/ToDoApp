cf1fe82aec551b482f2c8ce94d03feb4ea50df69
Said00510 <dylanrivas200206@gmail.com>
2025-01-10 18:07:51 -0500

    Importar las funciones desde el provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,19 @@
-import { useEffect, useState } from "react";
 import { TaskAdd } from "./components/TaskAdd.jsx";
-import "./App.css";
 import { DrawTask } from "./components/DrawTask/DrawTask.jsx";
 import { CardTask } from "./components/CardTask.jsx";
 import { CompletedTask } from "./components/DrawTask/CompletedTask.jsx";
 import { Header } from "./components/Header.jsx";
+import { TaskContext } from "./context/taskContext.jsx";
+import { useContext } from "react";
 
 
 function App() {
 
-  const [completedTasks, setCompletedTasks] = useState([]);
-  const [tasks, setTasks] = useState([]);
-
-  const deleteTask = (index) => {
-    const newTasks = [...tasks];
-    newTasks.splice(index, 1);
-    setTasks(newTasks);
-  };
-
-  const handleCompleted = (index) => {
-    const doneTask = tasks[index];
-    deleteTask(index);
-    setCompletedTasks([...completedTasks, doneTask]);
-   
-  };
-
-  const handleDeleteCompletedTask = (index) => {
-    const deletedCompletedTask = completedTasks[index];
-    setCompletedTasks(completedTasks.filter((_, i) => i !== index))
-    setTasks([...tasks, deletedCompletedTask])
-  }
-
-  useEffect(() => {
-    let data = localStorage.getItem("tasks");
-    let completedTasks = localStorage.getItem("completedTasks");
-    if (data) {
-      setTasks(JSON.parse(data));
-    }
-    if(completedTasks){
-      setCompletedTasks(JSON.parse(completedTasks));
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }, [tasks]);
-
-  useEffect(() => {
-    localStorage.setItem("completedTasks", JSON.stringify(completedTasks));
-} , [completedTasks])
-
-  //en react no se utilizan metodos mutables, por lo que no se puede utilizar push, pop, shift, unshift, etc, se usan metodos inmutables como map, filter, reduce, etc
-  //de esta manera se actualizan los elementos ... spread operator crea una copia de un array y agrega un nuevo elemento ejem: array = [...array, newElement], a = [1,2,3] b = [...a, 4] b = [1,2,3,4] el array a no se modifica solo se crea una copia con el nuevo elemento
-  const createNewTask = (task) => {
-    setTasks([...tasks, task]);
-  };
+  const {tasks , completedTasks, deleteTask, handleCompleted, handleDeleteCompletedTask} = useContext(TaskContext);
 
   return (
     <>
-      <main>
+      <main className="px-6">
         <Header />
 
         {tasks.length === 0 && completedTasks.length === 0 ? (
@@ -70,11 +25,10 @@ function App() {
                 key={index}
                 value={task}
                 tasks={tasks}
-                deleteTask={() => {deleteTask(index)}}
-                handleCompleted={() => {handleCompleted(index)}}
+                deleteTask={() => deleteTask(index)}
+                handleCompleted={() => handleCompleted(index)}
                 completedTasks={completedTasks}
                 handleDeleteCompletedTask={() => {handleDeleteCompletedTask(index)}}
-                taskCompleted={task}
               />
             );
           })
@@ -84,7 +38,7 @@ function App() {
           <CompletedTask completedTasks={completedTasks} handleDeleteCompletedTask={handleDeleteCompletedTask} />
         )}
       </main>
-      <TaskAdd createNewTask={createNewTask} />
+      <TaskAdd />
     </>
   );
 }
