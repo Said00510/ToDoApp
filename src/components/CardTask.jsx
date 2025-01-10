bdef7b2410571c4e21f2dce1183faa9a3cbdaa47
Said00510 <dylanrivas200206@gmail.com>
2025-01-09 19:39:10 -0500

    Cambiar texto de bienvenida

--- src/components/CardTask.jsx
+++ src/components/CardTask.jsx
@@ -4,14 +4,13 @@ export function CardTask() {
   return (
     <div className="flex justify-center items-center mt-7">
       <div className="max-w-[300px] rounded-2xl backdrop-blur-md text-white p-6">
-        <img className="mx-auto w-[150px] h-[150px]" src={Calendario} alt="" />
+        <img className="mx-auto w-[150px] h-[150px]" src={Calendario} alt="Imagen de un calendario" />
         <div className="flex justify-center flex-col gap-2 items-center">
           <h3 className="text-2xl text-center font-normal">
             Concéntrate en tu dia
           </h3>
           <p className="text-center">
-            Termina tus tareas con Mi Día,una lista que se actualiza todos los
-            dias.
+            Termina todas tus tareas diarias con Mi Día.
           </p>
         </div>
       </div>
